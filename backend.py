0e0843645f7f4717b22f9eeb1e19caba325128d9
Imed Bouchrika <imed@imed.ws>
2025-10-21 14:08:12 +0100

    adding a line of comment

--- backend.py
+++ backend.py
@@ -3,17 +3,27 @@ import json
 
 app = Flask(__name__)
 
-@app.route('/categories.get')
-def get_categories():
-   data=[{'id':1,'name':'Food'},{'id':2,'name':'Beverage'}]
-   return json.dumps(data)
+
+@app.route('/news.all.get')
+def get_news_all_articles():
+	data=[]
+	with open('news_data.json', 'r') as file:
+		data = json.load(file)
+	app.logger.debug('___________________'+str(data))
+	return json.dumps(data)
+
+
+@app.route('/news.categories.get')
+def get_news_categories():
+	data=[{'id':1,'name':'Sports'},{'id':2,'name':'Politics'},{'id':3,'name':'Education'}]
+	return json.dumps(data)
 
 
 @app.route('/')
 def index():
-   return 'Welcome ENSIA Students from Flask!'
+	return 'Welcome ENSIA Students from Flask!'
 
 if __name__ == "__main__":
-   app.run(port=8080)
+	app.run(port=8080)
 
 
